2e7096c3facb2deaa9677026d6b9b29114659c92
julian <jrdicesare@protonmail.com>
2020-10-14 12:46:16 -0600

    Set viewport for mobile devices

--- webapp/header.php
+++ webapp/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Julian DiCesare</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- TODO: vendor custom bootstrap -->
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
